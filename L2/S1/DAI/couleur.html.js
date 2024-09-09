8636fef7f5d2d4ab10654a9c599da57056c7ee34
Macxence Schneider <schneidermacxencepro@gmail.com>
2024-09-09 12:45:31 +0200

    ajout

---
+++ L2/S1/DAI/couleur.html.js
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>;
+
+
+
+
+<scrip>
+
+function {
+    let couleur = prompt('Donner la couleur du feu');
+
+    if (couleur == 'Vert');
+        
+        alert('Tu peux continuer');
+
+    else if (couleur == 'orange' || couleur == 'rouge'  )
+        alert('ARRETE TOI');
+    
+        else 
+            alert('Avance en faisant attention');
+}
+
+</scrip>
