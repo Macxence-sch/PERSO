3a7a1aedece10e982e95bbc4f0bde2ca0e802ea5
Macxence Schneider <schneidermacxencepro@gmail.com>
2024-09-15 23:14:51 +0200

    Pendue / Bug de fichier

--- C C++/JEUX/pendu.c
+++ C C++/JEUX/pendu.c
@@ -8,37 +8,44 @@
 struct mot{
     int id;
     int nb;
-    char mot[50]
+    char mot[50];
 };
 
 struct datamot {
     int points;
     int count;
-    struct mot mots[MAX_MOT]
+    struct mot mots[MAX_MOT];
 };
 
 void initialisation (struct datamot *motDB);
 void ajoutermot (struct datamot *motDB);
 void voirmot (struct datamot *motDB);
+void jeux (struct datamot *motDB);
 
 int main(){
     srand(time(NULL)); 
     struct datamot motDB;
 
     int points,b,choix;
-
-    char tab2[500];
-    int nb;
-    int nberreur;
-    int trouve;  
-    char lettre;  
-    int test,test2;
-    int id;
-    points=0;
     
-    nb=8;
+    FILE *file;
+    file = fopen(FILENAME_Mot, "r+");
+    fread(&motDB.points, sizeof(int), 1, file);
+    motDB.points= 0;
+    fseek(file, 0, SEEK_SET);
+    fwrite(&motDB.points, sizeof(int), 1, file);
+    fclose(file);
+    
+    
     b=0;
     while (b!=1){
+
+        FILE *file;
+        file = fopen(FILENAME_Mot, "r");
+        fread(&motDB.points, sizeof(int), 1, file);
+        points=motDB.points;
+        fclose(file);
+
         printf("\n1 - On fait un pendu ?\n");
         printf("2 - Fin de jeux ?\n");
         printf("3 - Gestion Mot \n"); //a faire avec fichier
@@ -49,78 +56,7 @@ int main(){
         switch (choix)
         {
         case (1):
-            
-            id = rand() % motDB.count;
-            if (id == 0) {
-                id = motDB.count;
-            }
-
-            nberreur = 0;
-            nb = motDB.mots[id].nb;
-
-            for (int i=0; i<nb; i++){
-                tab2[i] = '_';
-            }
-            
-            printf("\nOn commence : Le mot fait %d lettres \n",nb);
-
-            trouve=0;
-            while (nberreur <= 10 && trouve != 1){
-
-                for (int i=0; i<nb; i++){
-                    printf(" %c ",tab2[i]);
-                }                                             
-
-                printf("\nDonne une lettre : \n");
-                scanf(" %c",&lettre);
-
-                for (int i=0; i<nb; i++){
-                    if (motDB.mots[id].mot[i]==lettre){
-                        printf("\nUne lettre minimum trouvée \n");
-                        tab2[i]=motDB.mots[id].mot[i];
-
-                    }
-                }      
-
-                test=0;
-                for (int i=0; i<nb; i++){
-                    if (motDB.mots[id].mot[i]!=lettre){
-                        test = test +1;
-                    }
-                }
-
-                test2=0;
-                for (int i=0; i<nb; i++){
-                    if (tab2[i]==motDB.mots[id].mot[i]){
-                        test2=test2+1;
-                    }   
-                }
-
-                if (test==nb){
-                    printf("\nLa lettre ne fait pas partie du mot :(\n");
-                    nberreur=nberreur+1;
-                }
-
-                if (test2==nb){
-                    printf("Braaavooo tu as trouvé le mot !!!\n");
-                    trouve=1;
-                }
-
-
-                if (trouve!=1){
-                    printf("Nombre d'erreur (attention pas plus de 10) : %d\n",nberreur);
-                }
-            }
-            
-            printf("Le mot etait : \n");
-            for (int i=0; i<nb; i++){
-                    printf(" %c ",tab2[i]);
-            }
-
-            if (trouve == 1){
-                points=points+1;
-            }    
-              
+            jeux (&motDB);
             break; 
         case (2):
             b=1;
@@ -135,24 +71,14 @@ int main(){
                 scanf("%d",&choix);
 
                 switch (choix){
-                    case (1):
-                        
+                    case (1):                       
                         ajoutermot (&motDB);
-
-                        /*printf("\n Ton mot fait combien de lettre ?\n");
-                        scanf(" %d",&nb);
-                        for (int i = 0; i< nb;i++){
-                            printf("%d er/eme lettres : \n",i+1);
-                            scanf(" %c",&tab[i]);
-                        }*/
                         break;
                     case (2):
 
                         break;
-
                     case (3):  
                         voirmot (&motDB);
-
                         break;
                     case (4):
                         b=2;
@@ -166,7 +92,7 @@ int main(){
             b=1;
             break;
         case (5):
-            //initialisation (&motDB);
+            initialisation (&motDB);
             break;
         default:
             printf("Tu t'es trompé de numéro !\n");
@@ -215,6 +141,7 @@ void ajoutermot (struct datamot *motDB){
     printf("\nDonne ton mot : \n");
     scanf(" %s",&motDB->mots[iddd].mot);
     
+    
     int length = strlen(motDB->mots[iddd].mot);
     motDB->mots[iddd].nb = length;
     
@@ -233,8 +160,9 @@ void ajoutermot (struct datamot *motDB){
 
 void voirmot (struct datamot *motDB){
     FILE *file;
-    int iddd;
     file = fopen(FILENAME_Mot, "r");
+
+    int iddd;
 
     if (file == NULL) {
         	printf("Erreur lors de l'ouverture du fichier.\n");
@@ -245,21 +173,119 @@ void voirmot (struct datamot *motDB){
     fread(&motDB->count, sizeof(int), 1, file);
     iddd = motDB->count;
 
-    iddd = iddd+1;
+    //iddd = iddd  + 1;
     
     fseek(file, sizeof(int), SEEK_SET);
 
-    for (int i=2; i <= iddd; i++ ){
-        fread(&motDB->mots[iddd], sizeof(struct mot), 1, file);
-        //fread(&motDB->mots[iddd].id, sizeof(int), 1, file);
-        printf("\nID : %d",motDB->mots[iddd].id);
-        //fread(&motDB->mots[iddd].mot, sizeof(char), 1, file);
-        printf(" Mot : %s",motDB->mots[iddd].mot);
-        //fread(&motDB->mots[iddd].nb, sizeof(int), 1, file);
-        printf(" Nombre de lettre : %d",motDB->mots[iddd].nb);
+    for (int i=0; i <= iddd; i++ ){
+        fread(&motDB->mots[iddd + 1], sizeof(struct mot), 1, file);
+       
+        printf("\nID : %d",motDB->mots[iddd + 1].id);
+        
+        printf(" Mot : %s",motDB->mots[iddd + 1].mot);
+        
+        printf(" Nombre de lettre : %d",motDB->mots[iddd + 1].nb);
         printf(" \n");
     }
 
     fclose(file);
 
+}
+
+
+void jeux (struct datamot *motDB){
+    int id,nberreur,nb,trouve;
+    int test,test2;
+    char lettre;
+    char tab2[50];
+
+    FILE *file;
+    file = fopen(FILENAME_Mot, "r+");
+
+    if (file == NULL) {
+        	printf("Erreur lors de l'ouverture du fichier.\n");
+        	fclose(file);
+        	return;
+    }
+
+    fread(&motDB->count, sizeof(int), 1, file);
+
+
+    id = rand() % motDB->count;
+    if (id == 0) {
+        id = motDB->count;
+    }
+
+    fread(&motDB->mots[id + 1], sizeof(struct mot), 1, file);
+
+    nberreur = 0;
+    nb = motDB->mots[id].nb;
+
+    for (int i=0; i<nb; i++){
+        tab2[i] = '_';
+    }
+
+    printf("\nOn commence : Le mot fait %d lettres \n",nb);
+
+    trouve=0;
+    while (nberreur <= 10 && trouve != 1){
+
+        for (int i=0; i<nb; i++){
+            printf(" %c ",tab2[i]);
+        }                                             
+
+        printf("\nDonne une lettre : \n");
+        scanf(" %c",&lettre);
+
+        for (int i=0; i<nb; i++){
+            if (motDB->mots[id].mot[i]==lettre){
+                printf("\nUne lettre minimum trouvée \n");
+                tab2[i]=motDB->mots[id].mot[i];
+
+            }
+        }      
+
+        test=0;
+        for (int i=0; i<nb; i++){
+            if (motDB->mots[id].mot[i]!=lettre){
+                test = test +1;
+            }
+        }
+
+        test2=0;
+        for (int i=0; i<nb; i++){
+            if (tab2[i]==motDB->mots[id].mot[i]){
+                test2=test2+1;
+            }   
+        }
+
+        if (test==nb){
+            printf("\nLa lettre ne fait pas partie du mot :(\n");
+            nberreur=nberreur+1;
+        }
+
+        if (test2==nb){
+            printf("Braaavooo tu as trouvé le mot !!!\n");
+            trouve=1;
+        }
+
+
+        if (trouve!=1){
+            printf("Nombre d'erreur (attention pas plus de 10) : %d\n",nberreur);
+        }
+    }
+
+    printf("Le mot etait : \n");
+    for (int i=0; i<nb; i++){
+            printf(" %c ",tab2[i]);
+    }
+
+    if (trouve == 1){
+        fread(&motDB->points, sizeof(int), 1, file);
+        motDB->points=motDB->points+1;
+        fseek(file, 0, SEEK_SET);
+        fwrite(&motDB->points, sizeof(int), 1, file);
+    }
+
+    fclose(file);
 }
